c24d9b93996e9f5294c48cfc2b682c77a0661224
John Doe <johndoe@example.com>
2025-02-18 19:18:18 +0100

    u

--- build.sh
+++ build.sh
@@ -75,7 +75,8 @@ dnf5 -y install vdpauinfo \
                 libreoffice-TexMaths \
                 libreoffice \
                 tpm2-pkcs11 \
-                tpm2-openssl 
+                tpm2-openssl \
+                libvirt
 ### groups :
 dnf5 -y group install development-tools virtualization
 
